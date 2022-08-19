70b00bf58f1cf43d4c25025b0e2d0e1db4d4d149
benzed3 <benzed3@gmail.com>
2022-08-19 18:15:02 -0400

    deleting code to fix modal error

--- app.js
+++ app.js
@@ -28,33 +28,3 @@ $(document).ready(function () {
 
 });
 
-//For Trivia Game//
-$(document).ready(function () {
-
-    // Get the modal
-    var modalT = document.getElementById("myModalTrivia");
-
-    // Get the button that opens the modal
-    var trainClick = document.getElementById("triviaImg");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    trainClick.onclick = function () {
-        modalT.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modalT.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modalT.style.display = "none";
-        }
-    }
-
-});
