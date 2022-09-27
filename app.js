e34ea88b485b028eea91b5dd8ca7a6613b6279ff
benzed3 <benzed3@gmail.com>
2022-09-27 18:58:22 -0400

    still testing modal; problem with jquery

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
     $("#trainImg").click(function () {
+
         <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
             tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog">
